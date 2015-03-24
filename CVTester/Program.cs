59232ef21899210c8efb51a0a5bbd234cef6f12a
AriFreyr <haugurjr@gmail.com>
2015-03-24 13:33:28 +0000

    first

---
+++ CVTester/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CVTester
+{
+    class Program
+    {
+        const string GoogleApiKey = "AIzaSyC3E6KWp9rLiIt3mVG4CsABiZcafO7456A";
+        const string SearchEngineCx = "015363661733912743555:c60m6j8qy2o";
+        static void Main(string[] args)
+        {
+            try
+            {
+                var imageTypes = new[] {"car", "laptop", "phone"};
+                var requestUrl = CreateRequest(imageTypes[0]);
+                var response = MakeRequest(requestUrl);
+
+                foreach (var item in response.items)
+                {
+                    Console.WriteLine(item.link);
+                }
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static string CreateRequest(string query)
+        {
+            var reqeustString = "https://www.googleapis.com/customsearch/v1?key="
+                                + GoogleApiKey + "&cx=" + SearchEngineCx + "&q=" + query
+                                + "&searchType=image";
+
+            return reqeustString;
+        }
+
+        public static Response MakeRequest(string requestUrl)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(requestUrl);
+            using (var response = (HttpWebResponse) request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception(String.Format(
+                                            "Server error (HTTP {0}: {1}).",
+                                        response.StatusCode,
+                    response.StatusDescription));
+                }
+                var serializer = new JsonSerializer();
+
+                Response result;
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = (Response)serializer.Deserialize(reader, typeof(Response));
+                }
+
+                return result;
+
+            }
+        }
+    }
+}
---
+++ CVTester/Utilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Drawing;
+
+namespace CVTester
+{
+    public class Utilities
+    {
+        public static System.Drawing.Image GetImageFromUrl(string url)
+        {
+            var httpWebRequest = (HttpWebRequest) WebRequest.Create(url);
+
+            try
+            {
+                using (var httpWebResponse = (HttpWebResponse) httpWebRequest.GetResponse())
+                {
+                    using (var stream = httpWebResponse.GetResponseStream())
+                    {
+                        if (stream == null)
+                        {
+                            throw new ArgumentNullException(url);
+                        }
+                        return System.Drawing.Image.FromStream(stream);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception in GetImageFromUrl: " + e.Message);
+            }
+            return null;
+        }
+
+    }
+}
